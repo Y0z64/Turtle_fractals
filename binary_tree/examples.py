2fa357aad839d30c245a19806dab193ae9cc666a
Y0z64 <yairprogrammer@gmail.com>
2023-05-10 00:42:43 -0700

    Added functional fractal_canopy

--- binary_tree/examples.py
+++ binary_tree/examples.py
@@ -2,6 +2,11 @@
 import turtle
 import binary_tree as bt
 
+def hidden_fd(x):
+    turtle.penup()
+    turtle.fd(x)
+    turtle.pendown()
+
 # Proportions
 WITDH = 900
 HEIGHT = 900
@@ -10,45 +15,18 @@ HEIGHT = 900
 turtle.setup(WITDH, HEIGHT)
 turtle.seth(90)
 
-# Start
-turtle.tracer(0, 0)
-
-def hidden_fd(x):
-    turtle.penup()
-    turtle.fd(x)
-    turtle.pendown()
 
 # Up tree
-for i in range(4):
-    angle = 90
-    fractal_angle = 27
-    turtle.seth(angle*i)
-    bt.runner(bt.builder(10), 20, 27)
-    hidden_fd(250)
-    bt.runner(bt.builder(9), 17, 27)
-    bt.go_home()
-    hidden_fd(300)
-    direction = turtle.heading()
-    bt.runner(bt.builder(10), 50, 60)
-    bt.go_home()
-    turtle.seth(direction)
-    hidden_fd(300)
-    turtle.right(10)
-    bt.runner(bt.builder(10), 50, 60)
-    bt.go_home()
-    turtle.seth(direction)
-    hidden_fd(300)
-    turtle.left(10)
-    bt.runner(bt.builder(10), 50, 60)
-    bt.go_home()
-
+n = 3
+secuence = bt.builder(3)
+print(secuence)
+bt.runner(secuence, 100, 27)
 
 #Reset turtle
 bt.go_home()
 turtle.seth(90)
 
 # End
-turtle.update()
 turtle.exitonclick()
 
 # Caotic results 
--- binary_tree/fractal_canopy.py
+++ binary_tree/fractal_canopy.py
@@ -1,38 +1,68 @@
 import turtle
 
-screen = turtle.Screen()
-
-WITDH = 900
-HEIGHT = 900
-# Setup
-turtle.setup(WITDH, HEIGHT)
-turtle.seth(90)
-turtle.penup()
-turtle.goto(0, -HEIGHT/2)
-turtle.pendown()
-
-def branch(x, r):
-    turtle.fd(x*r)
-
-
-
-turtle.tracer(0, 0)
-# Code goes here
-stack = []
-n = 3
-for i in range(n):
-    x = 200
-    r = (5/8)
-
-    r = r**len(stack)
-    branch(x, r)
-    stack.append(turtle.pos())
+def reset(x, hd):
     turtle.penup()
-    turtle.fd(10)
+    turtle.goto(x)
     turtle.pendown()
+    turtle.seth(hd)
+
+def append_2_stack(stack):
+    stack.append((turtle.pos(), turtle.heading()))
+
+def segment(stack, stack_2, leng, l_theta, r_theta):
+    x, dir = stack.pop(-1)
+    reset(x, dir)
+
+    turtle.left(l_theta)
+    turtle.fd(leng)
+    append_2_stack(stack_2)
+
+    reset(x, dir)
+
+    turtle.right(r_theta)
+    turtle.fd(leng)
+    append_2_stack(stack_2)
+
+    
+
+# Setup
+WIDTH = 900
+HEIGHT = 900
+
+turtle.setup(WIDTH, HEIGHT)
+turtle.penup()
+turtle.goto(0, -HEIGHT/2+50)
+turtle.pendown()
+turtle.seth(90)
+
+
+# Code here
+turtle.tracer(0, 0)
+
+turtle.seth(90)
+n = 5
+x = 300
+l_theta = 45
+r_theta = 45
+r = 0.5
+
+# Define stack
+stack = []
+stack_2 = []
+
+# Trunk
+turtle.fd(x)
+append_2_stack(stack)
+ratio_counter = 1
+for _ in range(n):
+    ratio = r ** ratio_counter
+    if len(stack) == 0:
+        stack, stack_2 = stack_2, stack
+        ratio_counter = ratio_counter + 1
+    for _ in range(len(stack)):
+        segment(stack, stack_2, x*ratio, l_theta, r_theta)
 
 
 turtle.update()
-
 
 turtle.exitonclick()
